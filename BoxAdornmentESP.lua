9f12c669f36b3827db8c0444b33fa2e012232ce3
Neado_ <118765093+IGriffy@users.noreply.github.com>
2025-02-19 17:21:41 +0300

    Create BoxAdornmentESP.lua

---
+++ BoxAdornmentESP.lua
@@ -0,0 +1,60 @@
+local CoreGui = game:GetService("CoreGui")
+local Players = game:GetService("Players")
+local Player = Players.LocalPlayer
+
+local Character = Player.Character or Player.CharacterAdded:Wait()
+local RootPart = Character:FindFirstChild("HumanoidRootPart")
+local Humanoid = Character:FindFirstChild("Humanoid")
+
+local Folder = Instance.new("Folder")
+Folder.Name = "_BoxHandleAdornment_Players_"
+Folder.Parent = CoreGui
+
+_G.WhiteList = {"piska_bobrafsko", "KingOvCursed", "Crankbread8743", "Luffy_77787"}
+
+local function ChangeBHA(Object, Color)
+    local ObjectsFolder = Folder:FindFirstChild(Object.Name) or Instance.new("Folder", Folder)
+    ObjectsFolder.Name = Object.Name
+
+    for i, v in pairs(Object:GetChildren()) do
+        if v:IsA("Part") or v:IsA("MeshPart") then
+            local BoxAdornment = ObjectsFolder:FindFirstChild(Object.Name.."_"..v.Name) or Instance.new("BoxHandleAdornment", ObjectsFolder)
+            BoxAdornment.Size = v.Size
+            BoxAdornment.AlwaysOnTop = true
+            BoxAdornment.Transparency = 0.6
+            BoxAdornment.Adornee = v
+            BoxAdornment.Color3 = Color
+            BoxAdornment.ZIndex = 1
+            BoxAdornment.Name = Object.Name.."_"..v.Name
+        end
+    end
+end
+
+local function CheckWhiteList(Plr)
+    for _, name in ipairs(_G.WhiteList) do
+        if name == Plr.Name then
+            return true
+        end
+    end
+    return false
+end
+
+Players.PlayerRemoving:Connect(function(player)
+    local a = Folder:FindFirstChild(player.Name):Destroy()
+    if a then a:Destroy() end
+end)
+
+while true do
+    for _, Plr in Players:GetChildren() do
+        if Plr ~= Player and Plr.Character then
+            if CheckWhiteList(Plr) then
+                ChangeBHA(Plr.Character, Color3.fromRGB(255,255,0))
+            elseif Plr.TeamColor ~= Player.TeamColor then
+                ChangeBHA(Plr.Character, Color3.fromRGB(255,0,0))
+            elseif Plr.TeamColor == Player.TeamColor then
+                ChangeBHA(Plr.Character, Color3.fromRGB(0,0,255))
+            end
+        end
+    end
+    task.wait(1)
+end
