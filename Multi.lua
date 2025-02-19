081d034969f7bc1bc2195a5663db251dac82a598
Neado_ <118765093+IGriffy@users.noreply.github.com>
2025-02-19 22:44:50 +0300

    Update Multi.lua

--- Multi.lua
+++ Multi.lua
@@ -1,44 +1,53 @@
+if UniversalNeadoScriptExecuted then print("Universal script already executed") return end
+
+--------------------- Service's ---------------------
+
 local UIS = game:GetService("UserInputService")
 local Players = game:GetService("Players")
+local CoreGui = game:GetService("CoreGui")
+
+----------------------- Var's -----------------------
 
 local Player = Players.LocalPlayer
 local Character = Player.Character or Player.CharacterAdded:Wait()
 local Humanoid = Character:WaitForChild("Humanoid")
-local HumanoidRootPart = Character:WaitForChild("HumanoidRootPart")
+local RootPart = Character:FindFirstChild("HumanoidRootPart")
 local Mouse = Player:GetMouse()
 local Camera = workspace.CurrentCamera
 
-local CanCollideFalseList = {"HumanoidRootPart", "Left Leg", "Right Arm", "Right Leg"}
-local noClipEnabled = false
-local flying = false
-local keys = {a = false, d = false, w = false, s = false, e = false, q = false, z = false, shift = false}
+getgenv().CanCollideFalseList = {"HumanoidRootPart", "Left Leg", "Right Arm", "Right Leg"}
+getgenv().NoClipEnabled = false
+getgenv().Flying = false
+getgenv().FlySpeed = 0.5
+
+getgenv().IsPlusKeyHeld = false
+getgenv().IsMinusKeyHeld = false
+
+-------------------- Function's ---------------------
 
 local function TeleportToMouse()
 	local MousePos = Mouse.Hit.Position
-	Character:MoveTo(MousePos)
+	RootPart.CFrame = CFrame.new(MousePos+Vector3.new(0,3,0))*RootPart.CFrame.Rotation
 end
 
-local function toggleNoClip()
-	noClipEnabled = not noClipEnabled
+local function ToggleNoClip()
+	NoClipEnabled = not NoClipEnabled
 
-	for _, part in ipairs(Character:GetChildren()) do
-		if part:IsA("BasePart") then
-			if noClipEnabled then
-				part.CanCollide = false
+	for _, Part in ipairs(Character:GetChildren()) do
+		if Part:IsA("BasePart") then
+			if NoClipEnabled then
+				Part.CanCollide = false
 			else
-				part.CanCollide = not table.find(CanCollideFalseList, part.Name)
+				Part.CanCollide = not table.find(CanCollideFalseList, Part.Name)
 			end
 		end
 	end
 end
 
-local function startFlying()
+local function StartFlying()
 	repeat
-		local char = Player.Character
-		local rootPart:BasePart = char:WaitForChild('HumanoidRootPart')
-		local hum:Humanoid = char:WaitForChild('Humanoid')
-		hum.PlatformStand = true
-		local currentCF = rootPart.CFrame
+		Humanoid.PlatformStand = true
+		local CurrentCF = RootPart.CFrame
 
 		task.wait() do
 			local add = Vector3.new(0,0,0)
@@ -50,67 +59,92 @@ local function startFlying()
 			if UIS:IsKeyDown(Enum.KeyCode.E) then add += Camera.CFrame.UpVector end
 			if UIS:IsKeyDown(Enum.KeyCode.Q) then add -= Camera.CFrame.UpVector end
 
-			rootPart.AssemblyLinearVelocity = Vector3.zero
-			rootPart.AssemblyAngularVelocity = Vector3.zero
+			RootPart.AssemblyLinearVelocity = Vector3.zero
+			RootPart.AssemblyAngularVelocity = Vector3.zero
 
-			currentCF += add
-			rootPart.CFrame = CFrame.lookAt(
-				currentCF.Position,
-				currentCF.Position + (Camera.CFrame.LookVector * 2)
+			CurrentCF += add * FlySpeed
+			RootPart.CFrame = CFrame.lookAt(
+				CurrentCF.Position,
+				CurrentCF.Position + (Camera.CFrame.LookVector * 2)
 			)
 		end
-	until not flying 
-
+	until not Flying 
 	Humanoid.PlatformStand = false
 end
 
-Mouse.KeyDown:Connect(function(key)
-	if key == "x" then
-		flying = not flying
-		if flying then
-			startFlying()
+local ScreenGui = Instance.new("ScreenGui")
+ScreenGui.Parent = CoreGui
+local TextLabel = Instance.new("TextLabel")
+TextLabel.AnchorPoint = Vector2.new(0.5, 0.5)
+TextLabel.Position = UDim2.new(0.65, 0, 0.9, 0)
+TextLabel.Size = UDim2.new(0.125, 0, 0.1, 0)
+TextLabel.BackgroundTransparency = 1
+TextLabel.TextColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel.TextXAlignment = Enum.TextXAlignment.Left
+TextLabel.TextStrokeTransparency = 0
+TextLabel.TextSize = 15
+TextLabel.Parent = ScreenGui
+
+------------------- Connection's --------------------
+
+task.spawn(function()
+	while true do
+		if IsPlusKeyHeld == true then
+			FlySpeed = FlySpeed + 0.1
 		end
-	elseif key == "w" then
-		keys.w = true
-	elseif key == "s" then
-		keys.s = true
-	elseif key == "a" then
-		keys.a = true
-	elseif key == "d" then
-		keys.d = true
-	elseif key == "e" then
-		keys.e = true
-	elseif key == "q" then
-		keys.q = true
-	elseif key == "0" then
-		keys.shift = true
-	elseif key == "t" then
-		TeleportToMouse()
-	elseif key == "z" then
-		toggleNoClip()
+		if IsMinusKeyHeld == true then
+			FlySpeed = math.max(0.1, FlySpeed - 0.1)
+		end
+        TextLabel.Text = "Fly speed :  "..(string.format("%.1f", FlySpeed)).."\nNoclip......:  "..tostring(NoClipEnabled and "Enabled" or not NoClipEnabled and "Disabled")
+		task.wait(.15)
 	end
 end)
 
-Mouse.KeyUp:Connect(function(key)
-	if key == "w" then
-		keys.w = false
-	elseif key == "s" then
-		keys.s = false
-	elseif key == "a" then
-		keys.a = false
-	elseif key == "d" then
-		keys.d = false
-	elseif key == "e" then
-		keys.e = false
-	elseif key == "q" then
-		keys.q = false
-	elseif key == "0" then
-		keys.shift = false
+UIS.InputBegan:Connect(function(Input, GameProcessed)
+    if GameProcessed then return end
+	if Input.KeyCode == Enum.KeyCode.Equals then
+		IsPlusKeyHeld = true
+	elseif Input.KeyCode == Enum.KeyCode.KeypadPlus then
+		IsPlusKeyHeld = true
+	elseif Input.KeyCode == Enum.KeyCode.Minus then
+		IsMinusKeyHeld = true
+	elseif Input.KeyCode == Enum.KeyCode.KeypadMinus then
+		IsMinusKeyHeld = true
+	end
+    print("\nFly speed: "..FlySpeed.."\nInput key: "..Input.KeyCode.Name.."\nIs plus key held: "..tostring(IsPlusKeyHeld).."\nIs minus key held: "..tostring(IsPlusKeyHeld))
+end)
+
+UIS.InputEnded:Connect(function(Input, GameProcessed)
+    if GameProcessed then return end
+	if Input.KeyCode == Enum.KeyCode.Equals then
+		IsPlusKeyHeld = false
+	elseif Input.KeyCode == Enum.KeyCode.KeypadPlus then
+		IsPlusKeyHeld = false
+	elseif Input.KeyCode == Enum.KeyCode.Minus then
+		IsMinusKeyHeld = false
+	elseif Input.KeyCode == Enum.KeyCode.KeypadMinus then
+		IsMinusKeyHeld = false
+	end
+end)
+
+UIS.InputEnded:Connect(function(Input, GameProcessed)
+    if GameProcessed then return end
+	if Input.KeyCode == _G.BindFly then
+		Flying = not Flying
+		if Flying then
+			StartFlying()
+		end
+    elseif Input.KeyCode == _G.BindTeleport then
+		TeleportToMouse()
+	elseif Input.KeyCode == _G.BindNoclip then
+		ToggleNoClip()
 	end
 end)
 
 Player.CharacterAdded:Connect(function(newCharacter)
 	Character = newCharacter
 	Humanoid = Character:WaitForChild("Humanoid")
-	HumanoidRootPart = Character:WaitForChild("HumanoidRootPart")
+	RootPart = Character:WaitForChild("HumanoidRootPart")
 end)
+
+getgenv().UniversalNeadoScriptExecuted = true
