8781a91345e52f0f244deee0092e1c295a03493d
Neado_ <118765093+IGriffy@users.noreply.github.com>
2025-02-25 16:39:20 +0300

    Update Optimization.lua

--- Optimization.lua
+++ Optimization.lua
@@ -5,6 +5,7 @@ Light.Technology = Enum.Technology.Voxel
 Light.GlobalShadows = false
 
 local function ObjectOptimize(Object)
+    if Object:FindFirstChild("Humanoid") or Object.Parent:FindFirstChild("Humanoid") then return end
     if Object:IsA("Part") or Object:IsA("MeshPart") or Object:IsA("WedgePart") or Object:IsA("UnionOperation") then
         Object.Material = Enum.Material.SmoothPlastic
     elseif Object:IsA("Model") then
