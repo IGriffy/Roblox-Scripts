7defef41ab8d92bd7498c19828f96799102a0b8a
Neado_ <118765093+IGriffy@users.noreply.github.com>
2025-02-26 22:44:06 +0300

    Update Optimization.lua

--- Optimization.lua
+++ Optimization.lua
@@ -1,8 +1,17 @@
+----------------------- Check -----------------------
+
 if OptimizationNeadoScrptExecuted then print("Optimization script already executed") return end
 
+--------------------- Service's ---------------------
+
 local Light = game:GetService("Lighting")
+
+---------------------- Other -----------------------
+
 Light.Technology = Enum.Technology.Voxel
 Light.GlobalShadows = false
+
+-------------------- Function's ---------------------
 
 local function ObjectOptimize(Object)
     if Object:IsA("Part") or Object:IsA("MeshPart") or Object:IsA("WedgePart") or Object:IsA("UnionOperation") then
@@ -14,12 +23,16 @@ local function ObjectOptimize(Object)
     end
 end
 
-for _, obj in pairs(workspace:GetDescendants()) do
-    ObjectOptimize(obj)
-end
+------------------- Connection's --------------------
 
 workspace.ChildAdded:Connect(function(obj)
     ObjectOptimize(obj)
 end)
+
+---------------------- Other -----------------------
+
+for _, obj in pairs(workspace:GetDescendants()) do
+    ObjectOptimize(obj)
+end
 
 getgenv().OptimizationNeadoScrptExecuted = true
