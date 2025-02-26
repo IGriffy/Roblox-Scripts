547c9560c20f8d382653fa7872381a021fe4cf84
Neado_ <118765093+IGriffy@users.noreply.github.com>
2025-02-26 22:53:28 +0300

    Update HighlightESP.lua

--- HighlightESP.lua
+++ HighlightESP.lua
@@ -1,12 +1,20 @@
+----------------------- Check's ---------------------
+
 if HighlightESPNeadoScriptExecuted then print("Highlight ESP script already executed") return end
+
+--------------------- Service's ---------------------
 
 local CoreGui = game:GetService("CoreGui")
 local Players = game:GetService("Players")
 local Player = Players.LocalPlayer
 
+----------------------- Var's -----------------------
+
 local Folder = Instance.new("Folder")
 Folder.Name = "_Highlight_Players_"
 Folder.Parent = CoreGui
+
+-------------------- Function's ---------------------
 
 local function ChangeHighlight(Object, FillColor, FillTransparency, OutlineColor, OutlineTransparency)
     local Highlight = Folder:FindFirstChild(Object.Name) or Instance.new("Highlight", Folder)
@@ -27,19 +35,23 @@ local function CheckWhiteList(Plr)
     return false
 end
 
-while true do
-    for _, Plr in Players:GetChildren() do
-        if Plr ~= Player and Plr.Character then
-            if CheckWhiteList(Plr) then
-                ChangeHighlight(Plr.Character, Color3.fromRGB(255,255,0), 0.5, Color3.fromRGB(255,255,255), 0.5)
-            elseif Plr.TeamColor ~= Player.TeamColor then
-                ChangeHighlight(Plr.Character, Color3.fromRGB(255,0,0), 0.5, Color3.fromRGB(255,255,255), 0.5)
-            elseif Plr.TeamColor == Player.TeamColor then
-                ChangeHighlight(Plr.Character, Color3.fromRGB(0,0,255), 0.5, Color3.fromRGB(255,255,255), 0.5)
+---------------------- Other -----------------------
+
+task.spawn(function()
+    while true do
+        for _, Plr in Players:GetChildren() do
+            if Plr ~= Player and Plr.Character then
+                if CheckWhiteList(Plr) then
+                    ChangeHighlight(Plr.Character, Color3.fromRGB(255,255,0), 0.5, Color3.fromRGB(255,255,255), 0.5)
+                elseif Plr.TeamColor ~= Player.TeamColor then
+                    ChangeHighlight(Plr.Character, Color3.fromRGB(255,0,0), 0.5, Color3.fromRGB(255,255,255), 0.5)
+                elseif Plr.TeamColor == Player.TeamColor then
+                    ChangeHighlight(Plr.Character, Color3.fromRGB(0,0,255), 0.5, Color3.fromRGB(255,255,255), 0.5)
+                end
             end
         end
+        task.wait(1)
     end
-    task.wait(1)
-end
+end)
 
 getgenv().HighlightESPNeadoScriptExecuted = true
