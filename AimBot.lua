3bd8cc134e58e7c27b112a940c197654a748ce1d
Neado_ <118765093+IGriffy@users.noreply.github.com>
2025-02-26 23:11:19 +0300

    Update AimBot.lua

--- AimBot.lua
+++ AimBot.lua
@@ -1,4 +1,12 @@
-if ScriptExecuted then print("AimBot already executed") return end
+----------------------- Check's ---------------------
+
+if getgenv().ScriptExecuted then print("AimBot already executed") return end
+if _G.BindAimEnabled then print("Bind 'BindAimEnabled' not founded") return end
+if _G.HoldAimBind then print("Bind 'HoldAimBind' not founded") return end
+if _G.CircleRadius then print("not founded 'CircleRadius'") return end
+if _G.TargetPart then print("not founded 'TargetPart'") return end
+if _G.WhiteList then print("not founded 'WhiteList'") return end
+if _G.TeamCheck then print("not founded 'TeamCheck'") return end
 
 ---------------------- Config -----------------------
 
@@ -16,7 +24,6 @@ local CoreGui = game:GetService("CoreGui")
 ----------------------- Var's -----------------------
 
 local Player = Players.LocalPlayer
-local PlayerGui = Player.PlayerGui
 local Character = Player.Character or Player.CharacterAdded:Wait()
 local RootPart = Character:FindFirstChild("HumanoidRootPart")
 
@@ -25,34 +32,6 @@ local Camera = workspace.CurrentCamera
 
 -------------------- Function's ---------------------
 
-local function SwitchScriptEnabled()
-    getgenv().ScriptEnabled = not getgenv().ScriptEnabled
-end
-
-local function OnMouseButton1Down()
-    if not getgenv().AimEnabled then
-        getgenv().AimEnabled = true
-    end
-end
-
-local function OnMouseButton1Up()
-    if getgenv().AimEnabled then
-        getgenv().AimEnabled = false
-    end
-end
-
-local function OnKeyUp()
-    if not getgenv().AimEnabled then
-        getgenv().AimEnabled = true
-    end
-end
-
-local function OnKeyDown()
-    if getgenv().AimEnabled then
-        getgenv().AimEnabled = false
-    end
-end
-
 local function CheckWhiteList(Plr)
     for _, name in ipairs(_G.WhiteList) do
         if name == Plr.Name then
@@ -60,26 +39,6 @@ local function CheckWhiteList(Plr)
         end
     end
     return false
-end
-
-local function ReturnTargetPart()
-    if _G.TargetPart == "Random" then
-        return math.random(0, 1) == 0 and "Head" or "Torso"
-    else
-        return _G.TargetPart
-    end
-end
-
-local function AimAt(Target)
-    if Target.Parent:FindFirstChild("Humanoid") and Target.Parent.Humanoid.Health ~= 0 then
-        local Direction = (Target.Position - Camera.CFrame.Position).Unit
-        Camera.CFrame = CFrame.new(Camera.CFrame.Position, Camera.CFrame.Position + Direction)
-    end
-end
-
-local function ReturnCurrentCenter()
-    local ViewportSize = Camera.ViewportSize
-    return ViewportSize.X / 2, ViewportSize.Y / 2
 end
 
 local function GetClosestPlayerToCursor()
@@ -107,7 +66,55 @@ local function GetClosestPlayerToCursor()
     return ClosestPlayer
 end
 
-------------------- Connection's --------------------
+local function ReturnTargetPart()
+    if _G.TargetPart == "Random" then
+        return math.random(0, 1) == 0 and "Head" or "Torso"
+    else
+        return _G.TargetPart
+    end
+end
+
+local function AimAt(Target)
+    if Target.Parent:FindFirstChild("Humanoid") and Target.Parent.Humanoid.Health ~= 0 then
+        local Direction = (Target.Position - Camera.CFrame.Position).Unit
+        Camera.CFrame = CFrame.new(Camera.CFrame.Position, Camera.CFrame.Position + Direction)
+    end
+end
+
+local function ReturnCurrentCenter()
+    local ViewportSize = Camera.ViewportSize
+    return ViewportSize.X / 2, ViewportSize.Y / 2
+end
+
+local function OnMouseButton1Up()
+    if getgenv().AimEnabled then
+        getgenv().AimEnabled = false
+    end
+end
+
+local function OnMouseButton1Down()
+    if not getgenv().AimEnabled then
+        getgenv().AimEnabled = true
+    end
+end
+
+local function OnKeyUp()
+    if not getgenv().AimEnabled then
+        getgenv().AimEnabled = true
+    end
+end
+
+local function OnKeyDown()
+    if getgenv().AimEnabled then
+        getgenv().AimEnabled = false
+    end
+end
+
+local function SwitchScriptEnabled()
+    getgenv().ScriptEnabled = not getgenv().ScriptEnabled
+end
+
+---------------------- Other -----------------------
 
 local CenterX, CenterY = ReturnCurrentCenter()
 
@@ -118,18 +125,6 @@ Circle.Thickness = 2
 Circle.Radius = _G.CircleRadius
 Circle.Color = Color3.fromRGB(255, 100, 255)
 Circle.Visible = true
-
-Camera:GetPropertyChangedSignal("ViewportSize"):Connect(function()
-    local CenterX, CenterY = ReturnCurrentCenter()
-    Circle:Remove()
-    getgenv().Circle = Drawing.new("Circle")
-    Circle.Position = Vector2.new(CenterX, CenterY)
-    Circle.Transparency = 1
-    Circle.Thickness = 2
-    Circle.Radius = _G.CircleRadius
-    Circle.Color = Color3.fromRGB(255, 100, 255)
-    Circle.Visible = true
-end)
 
 task.spawn(function()
     while true do
@@ -146,6 +141,20 @@ task.spawn(function()
     end
 end)
 
+------------------- Connection's --------------------
+
+Camera:GetPropertyChangedSignal("ViewportSize"):Connect(function()
+    local CenterX, CenterY = ReturnCurrentCenter()
+    Circle:Remove()
+    getgenv().Circle = Drawing.new("Circle")
+    Circle.Position = Vector2.new(CenterX, CenterY)
+    Circle.Transparency = 1
+    Circle.Thickness = 2
+    Circle.Radius = _G.CircleRadius
+    Circle.Color = Color3.fromRGB(255, 100, 255)
+    Circle.Visible = true
+end)
+
 RunService.RenderStepped:Connect(function()
     if getgenv().ScriptEnabled and getgenv().AimEnabled then
         local TargetPlr = GetClosestPlayerToCursor()
@@ -158,22 +167,24 @@ RunService.RenderStepped:Connect(function()
     end
 end)
 
-Mouse.Button1Down:Connect(OnMouseButton1Down)
-Mouse.Button1Up:Connect(OnMouseButton1Up)
-
-UIS.InputBegan:Connect(function(Input, gameProcessed)
-    if not gameProcessed and Input.KeyCode == Enum.KeyCode.X then
-        OnKeyUp()
-    end
-end)
-
 UIS.InputEnded:Connect(function(Input, gameProcessed)
     if not gameProcessed and Input.KeyCode == _G.BindAimEnabled then
         SwitchScriptEnabled()
     end
-    if not gameProcessed and Input.KeyCode == Enum.KeyCode.X then
+    if not gameProcessed and Input.KeyCode == _G.HoldAimBind then
         OnKeyDown()
     end
 end)
+
+UIS.InputBegan:Connect(function(Input, gameProcessed)
+    if not gameProcessed and Input.KeyCode == _G.HoldAimBind then
+        OnKeyUp()
+    end
+end)
+
+Mouse.Button1Down:Connect(OnMouseButton1Down)
+Mouse.Button1Up:Connect(OnMouseButton1Up)
+
+---------------------- Other -----------------------
 
 getgenv().ScriptExecuted = true
